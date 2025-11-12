cf54802ba909a1112cb355f346aa30d06677653b
RyanPy <ryanbayu1233@gmail.com>
2025-11-12 17:32:57 +0700

    Add files via upload

---
+++ Javascript/Belajar/latihan1.js
@@ -0,0 +1,7 @@
+// AWAL, INPUT
+
+const nama = prompt("Siapa namamu: ");
+console.log("Halo " + nama);
+
+const umur = prompt("Umur kamu: ");
+console.log(`Umur kamu: ${umur} `);
---
+++ Javascript/Belajar/latihan2.js
@@ -0,0 +1,12 @@
+// VARIABEL DAN INPUT
+
+let x = prompt("Masukkan angka: ");
+if (x >= 90) {
+    console.log(`${x} = Grade A`);
+} else if (x >= 80) {
+    console.log(`${x} = Grade B`);
+} else if (x >= 65) {
+    console.log(`${x} = Grade C`);
+} else {
+    console.log(`${x} = Grade D`);
+}
